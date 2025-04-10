ee2e8b928641b80bdb2fa60b41d5ab9e75675572
Alish Palmos <alish.palmos@insmed.com>
2025-04-10 10:09:46 +0100

    add dedeup fix

--- makebgen/deduplication/deduplication.py
+++ makebgen/deduplication/deduplication.py
@@ -238,7 +238,7 @@ def remove_bcf_duplicates(query_string: str, vcf_prefix: Path,
     if "|" in query_string:
         raise ValueError("OR operator '|' not supported")
 
-    cmd = f'bcftools view --threads 2 -e \'{query_string}\' -Ob -o /test/{vcf_prefix}.deduped.bcf /test/{vcf_prefix}.bcf'
+    cmd = f'bcftools view --threads 2 -e \'{query_string}\' -Ob -o /test/{vcf_prefix}.deduped.bcf /test/{vcf_prefix}.missingness_filtered.bcf'
     cmd_exec.run_cmd_on_docker(cmd)
 
     # And rename the deduped bcf to match the final one we want to output
